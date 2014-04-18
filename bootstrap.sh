06ee9341910d0679792d2d54a03a6e0eb2b7e05b
Jack Lawson <ajacksified@gmail.com>
2014-04-18 14:24:02 -0700

    Super simple vagrant setup for running reddit.

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install curl
+
+wget https://raw.github.com/reddit/reddit/master/install-reddit.sh
+chmod +x install-reddit.sh
+./install-reddit.sh
+
+# replace the folder from install-reddit with our local folder
+
