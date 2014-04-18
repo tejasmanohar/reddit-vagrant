6dd4d9fc740d07ea02a0324b98bc7021173b35e2
Jack Lawson <ajacksified@gmail.com>
2014-04-18 14:39:56 -0700

    Update vagrant config files

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ sudo apt-get -y install curl
 
 wget https://raw.github.com/reddit/reddit/master/install-reddit.sh
 chmod +x install-reddit.sh
-./install-reddit.sh
+sudo ./install-reddit.sh
 
 # replace the folder from install-reddit with our local folder
 
